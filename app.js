7ca48e6b69c75d6c294b850e3b018d31cf523fe6
Alyssa <sanchezaeinstein@gmail.com>
2023-05-17 13:38:17 +0800

    1st commit

---
+++ app.js
@@ -0,0 +1,58 @@
+let homeElement = document.getElementById('home-count-element')
+let guestElement = document.getElementById('guest-count-element')
+let guest = document.getElementById('guest')
+let homeCount = 0
+let guestCount = 0
+
+// home
+function homeAddOne() {
+	homeCount += 1
+	homeElement.textContent = homeCount
+	updateCounts()
+}
+function homeAddTwo() {
+	homeCount += 2
+	homeElement.textContent = homeCount
+	updateCounts()
+}
+function homeAddThree() {
+	homeCount += 3
+	homeElement.textContent = homeCount
+	updateCounts()
+}
+// guest
+function guestAddOne() {
+	guestCount += 1
+	guestElement.textContent = guestCount
+	updateCounts()
+}
+function guestAddTwo() {
+	guestCount += 2
+	guestElement.textContent = guestCount
+	updateCounts()
+}
+function guestAddThree() {
+	guestCount += 3
+	guestElement.textContent = guestCount
+	updateCounts()
+}
+
+// Function to update the counts and styles
+function updateCounts() {
+	homeElement.textContent = homeCount
+	guestElement.textContent = guestCount
+
+	if (homeCount > guestCount) {
+		homeElement.style.color = 'red'
+		guestElement.style.color = ''
+	} else if (guestCount > homeCount) {
+		guestElement.style.color = 'red'
+		homeElement.style.color = ''
+	} else if (homeCount === guestCount) {
+		homeElement.style.color = ''
+		guestElement.style.color = ''
+	} else {
+		homeElement.style.backgroundColor = ''
+		guestElement.style.backgroundColor = ''
+	}
+}
